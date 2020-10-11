c7d07578797041ca36a1f43ca40fe3ad5e66e50a
Robert Preda <predarobert7@gmail.com>
2020-10-11 15:12:23 +0300

    Added color changing button

--- app/src/main/java/com/example/applab2/MainActivity.java
+++ app/src/main/java/com/example/applab2/MainActivity.java
@@ -6,23 +6,59 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private EditText name;
     private Button bClick;
+    private Spinner colorSpinner;
+
+    private Map<String, Integer> colorMap = new HashMap<String, Integer>();
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id){
+        String selectedColor = parent.getItemAtPosition(pos).toString();
+
+        this.bClick.setBackgroundColor(colorMap.get(selectedColor));
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> parent)
+    {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        name = (EditText)findViewById(R.id.eName);
-        bClick = (Button)findViewById(R.id.bGreet);
+        name = findViewById(R.id.eName);
+        bClick = findViewById(R.id.bGreet);
+        colorSpinner = findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.colous_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        colorSpinner.setAdapter(adapter);
+        colorSpinner.setOnItemSelectedListener(this);
+
+        colorMap.put("Red", Color.RED);
+        colorMap.put("Blue", Color.BLUE);
+        colorMap.put("Magenta", Color.MAGENTA);
+        colorMap.put("Green",Color.GREEN);
+
+
     }
 
     public void click(View view)
